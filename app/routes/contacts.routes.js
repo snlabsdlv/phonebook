9fe625b736a8e2006c2be078f33e5a3b5df66325
snladvs <ibrahim@ialabs.it>
2022-02-07 14:30:28 +0100

    clean project

--- app/routes/contacts.routes.js
+++ app/routes/contacts.routes.js
@@ -3,12 +3,10 @@ const contactsRouter = express.Router();
 const contactsController = require('../controllers/contacts.controller');
 contactsRouter.use(express.json());
 
-
 contactsRouter.get('/', async (req, res) => {
     try {
         const queryParams = req.query;
         const contact = await contactsController.getAllContacts(queryParams);
-
         if (!contact) {
             return res.status(404).json([]);
         }
@@ -18,17 +16,14 @@ contactsRouter.get('/', async (req, res) => {
     }
 });
 
-
-
 contactsRouter.get('/search', async (req, res) => {
-
     try {
         if (Object.keys(req.query).length === 0) {
             return res.status(400).json({ error: 'Missing Search Params' });
         }
         const searchParams = { ...req.query };
         const contact = await contactsController.searchContact(searchParams);
-        console.log('contactsRouter SEARCH  ', contact);
+
         if (!contact) {
             return res.status(404).json([]);
         }
@@ -38,19 +33,14 @@ contactsRouter.get('/search', async (req, res) => {
     }
 });
 
-
-
-
 contactsRouter.get('/:id', async (req, res) => {
     try {
         const contact = await contactsController.getContactById(req.params.id);
         if (!contact) {
-            console.log(`! getContactById contact  --> ${contact}`);
             return res.status(404).json([]);
         }
         return res.status(200).json(contact);
     } catch (error) {
-        console.log(`get ERROR --> ${error}`);
         return res.status(500).json({ error: error });
     }
 });
@@ -61,13 +51,12 @@ contactsRouter.post('/', async (req, res) => {
             return res.status(400).json({ error: 'Missing Body' });
         }
         const contact = await contactsController.newContact(req.body);
-        
+
         if (!contact || contact.hasOwnProperty('errors')) {
             return res.status(400).json({ error: 'Error Adding Contact' });
         }
         return res.status(201).json({ success: 'Created', id: contact._id });
     } catch (error) {
-        console.log('contactsRouter --> ERROR', error);
         return res.status(500).json({ error: error });
     }
 });
@@ -82,8 +71,7 @@ contactsRouter.put('/:id', async (req, res) => {
         }
         const contact = await contactsController.updateById(req.params.id, req.body);
         if (!contact) {
-            // console.log(`! updateById contact  --> ${contact}`);
-            return res.status(404).json({ error: 'Error Updating Contact', 'id': req.params.id });
+            return res.status(404).json({ error: 'Error Updating Contact', id: req.params.id });
         }
         return res.status(200).json(contact);
     } catch (error) {
@@ -98,12 +86,10 @@ contactsRouter.delete('/:id', async (req, res) => {
         }
         const contact = await contactsController.deleteById(req.params.id);
         if (!contact || Object.keys(contact).length === 0) {
-            // console.log('delete() contact ', contact);
             return res.status(404).json({ error: 'Not Found', id: req.params.id });
         }
         return res.status(200).json({ success: 'Deleted', id: contact._id });
     } catch (error) {
-        console.log(`delete error  --> ${error}`);
         return res.status(500).json({ error: error });
     }
 });
--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/', phoneRouter);
 
 app.use((err, req, res, next) => {
-    res.header('Content-Type', 'application/json');
-    res.status(422).send({ error: err.message });
+  res.header('Content-Type', 'application/json');
+  res.status(422).send({ error: err.message });
 });
 
 app.listen(process.env.PORT || 4000, () => {
-    console.log('Node.js server listening on port 4000 ');
+  console.log('Node.js server listening on port 4000 ');
 });
